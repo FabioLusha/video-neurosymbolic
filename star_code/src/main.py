24dc4059a7a7a547ed19dd777d30510eaf8c42d1
fabiolusha <fabio.lusha@mail.polimi.it>
2025-02-20 16:45:42 +0100

    Update run parameters for Llama8B and Deepseek7B

--- star_code/src/main.py
+++ star_code/src/main.py
@@ -26,14 +26,15 @@ def main():
     OLLAMA_URL = os.getenv('OLLAMA_URL', 'http://localhost:11434')
     ollama = OllamaRequestManager(
         base_url=OLLAMA_URL, 
+        # model='llama3.1:8b',
         # model='llama3.2',
         # model='phi3:3.8b',
-        model='deepseek-r1:1.5b',
+        model='deepseek-r1:7b',
         system=mcq_system_prompt,
         options={
-            'num_ctx': 10240,     # increasing the context window
+            'num_ctx': 20480,     # increasing the context window
             'temperature': 0.1,   # less createive and more focuesed generation (default: 0.8)
-            'num_predict': 4096   # let's check if fixing a number of max output token fixes the bug
+            'num_predict': 10240   # let's check if fixing a number of max output token fixes the bug
         }
         ) 
     
@@ -45,7 +46,7 @@ def main():
     
     # start from where the server crashed (repeat the last generation to test start parm
     # actually works)
-    prompts = list(prompt_generator.generate(mcq_pformat, start=3760, mcq=True))
+    prompts = list(prompt_generator.generate(mcq_pformat, mcq=True))
     # generate responses
     ollama.batch_requests(
         prompts=prompts,
