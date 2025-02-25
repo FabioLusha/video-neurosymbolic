5a8a70672e379745070b98a297fab384c15033b3
Fabio Lusha <fabio.lusha@mail.polimi.it>
2025-02-25 11:35:14 +0100

    Add test case for PromptFormatters

---
+++ star_code/test/test_formatters.py
@@ -0,0 +1,44 @@
+import unittest
+import random
+import json
+import sys
+
+sys.path.append('../src')
+
+
+# noqa: E402 - disables the warning for this line
+from ollama_manager import STARPromptGenerator  # noqa: E402
+import prompt_formatters as pf  # noqa: E402
+
+SEED = 11270525022025
+random.seed(SEED)
+
+
+class PromptFormatterTest(unittest.TestCase):
+    def __init__(self):
+        input_filename = '../data/datasets/STAR_QA_and_stsg_val.json'
+
+        with open(input_filename, 'r') as in_file:
+            q_stsg_data = json.load(in_file)
+            idx = random.randint(0, len(q_stsg_data))
+            self.sample = q_stsg_data[idx]
+
+    def test_simple_prompt(self):
+        prompt_format = "QUESTION: {question}\n"\
+                        "SPATIO-TEMPORAL SCENE-GRAPH: {stsg}"
+
+        question = self.sample['question']
+        stsg = self.sample['stsg']
+
+        gt_prompt = prompt_format.format(
+            question=question,
+            stsg=stsg
+        )
+
+        test_prompt = pf.OpenEndedPrompt(prompt_format).format(self.sample)
+
+        assert test_prompt == gt_prompt
+
+
+if __name__ == "__main__":
+    unittest.main()
