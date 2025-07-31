6bb9c0426d99df98d2b3d1bb52c0151b9c7ccfd6
fabiolusha <fabio.lusha@mail.polimi.it>
2025-07-31 09:10:35 +0000

    Add input prompt to the final response of the pipeline

--- star_code/scripts/gemini_batch_processing.py
+++ star_code/scripts/gemini_batch_processing.py
@@ -271,7 +271,34 @@ async def zero_shot_cot_batch_pipeline(client, model_name, reply_file, *input_fi
             new_batch_paths.append(str(out_fpath))
 
     final_results = await batch_processor(client, model_name, *new_batch_paths)
-    return final_results
+    # Create a file with all teh parts to infer the chat_history
+
+    final_paths = []
+    for i, result in enumerate(final_results):
+        if isinstance(result, Exception):
+            logger.warning(
+                f"The batch job for {input_files[i]} failed. Skipping this chunk"
+            )
+            continue
+
+        else:
+            input_fpath = Path(input_files[i])
+            response_fpath = Path(result)
+            with input_fpath.open("r") as f:
+                input_batch = [json.loads(line) for line in f.readlines()]
+            with response_fpath.open("r") as f:
+                out_batch = [json.loads(line) for line in f.readlines()]
+
+            new_batch = await append_response_to_query(input_batch, out_batch)
+
+            out_fpath = input_fpath.with_stem(f"{input_fpath.stem}_chat_history")
+            with out_fpath.open("w") as f:
+                for entry in new_batch:
+                    f.write(json.dumps(entry) + "\n")
+
+            final_paths.append(str(out_fpath))
+
+    return final_paths
 
 
 if __name__ == "__main__":
